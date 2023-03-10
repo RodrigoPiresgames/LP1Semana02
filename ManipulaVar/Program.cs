a98503968b6f947aa9d543db354baf36c9b0e808
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-10 10:48:26 +0000

    First step done

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                int x = int.Parse(arg);
+                Console.WriteLine($"{x++ - x}");
+            }
+        }
+    }
+}
