bc7eeb145d1d7ef4385156ff04e4976f6828e57e
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-10 13:37:44 +0000

    Switch sintaxe por padroes

---
+++ GranpaPatternSwitch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GranpaPatternSwitch
+{
+    class Program
+    {
+
+            private static void Main(string[] args)
+            {
+
+                Console.Write("Place your question? ");
+                string question = Console.ReadLine();
+
+                string response = question switch
+                {
+                    "How are you?" => response = "I'm fine, thank you",
+
+                    "What's your name?" => response = "Gran'pa",
+
+                    "What's your mission?" => response = "Destroy mankind!",
+
+                    _ => response = "You got me, I'm not THAT smart!",
+                };
+
+                Console.WriteLine(response);
+            }
+    }
+}
