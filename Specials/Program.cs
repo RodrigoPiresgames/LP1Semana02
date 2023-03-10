4c449ae0491c467bc84b3fea9062c076346251f1
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-10 11:31:28 +0000

    First step done

---
+++ Specials/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{sbyte.MaxValue} {sbyte.MinValue}");
+            Console.WriteLine($"{byte.MaxValue} {byte.MinValue}");
+            Console.WriteLine($"{short.MaxValue} {short.MinValue}");
+            Console.WriteLine($"{ushort.MaxValue} {ushort.MinValue}");
+            Console.WriteLine($"{int.MaxValue} {int.MinValue}");
+            Console.WriteLine($"{uint.MaxValue} {uint.MinValue}");
+            Console.WriteLine($"{long.MaxValue} {long.MinValue}");
+            Console.WriteLine($"{ulong.MaxValue} {ulong.MinValue}");
+            Console.WriteLine($"{float.MaxValue} {float.MinValue}");
+            Console.WriteLine($"{double.MaxValue} {double.MinValue}");
+            Console.WriteLine($"{decimal.MaxValue} {decimal.MinValue}");
+            Console.WriteLine($"{char.MaxValue} {char.MinValue}");
+        }
+    }
+}
