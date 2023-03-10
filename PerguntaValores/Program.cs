d6420d6f9e67f86f5a99d465f7b85092bbc4c9de
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-10 10:57:03 +0000

    Fixed ManipularVar bug

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
