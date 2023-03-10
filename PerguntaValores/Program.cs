8485ed5e6b1c08735619ebbc2d7ff5b9bd31fe84
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-10 11:11:34 +0000

    Second step done

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -15,7 +15,7 @@ namespace PerguntaValores
 
             double pi = 3.1415926;
             Console.WriteLine($"{ pi * (r * r) * a}");
-
+            Console.WriteLine($"{ (2 * pi * r) * (r + a)}");
 
         }
     }
