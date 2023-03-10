4764e22bc39edc86c0b4776e23b42bdda8507d44
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-10 11:09:00 +0000

    First step done

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -6,7 +6,17 @@ namespace PerguntaValores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Gimme a");
+            string str = Console.ReadLine();
+            float a = float.Parse(str);
+            Console.WriteLine("Gimme r");
+            str = Console.ReadLine();
+            float r = float.Parse(str);
+
+            double pi = 3.1415926;
+            Console.WriteLine($"{ pi * (r * r) * a}");
+
+
         }
     }
 }
