60c420975ae11df951efe0ce8c72a552fb98c133
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-02-28 13:39:22 +0000

    SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            sbyte n = sbyte.Parse(Console.ReadLine());
+            Console.WriteLine($"{ n-- }");
+            Console.WriteLine($"{ n++ }");
+            
+        }
+    }
+}
