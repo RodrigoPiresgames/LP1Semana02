ba97a7740ccad38341a083b504998a656f7b93c8
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-02-28 14:13:44 +0000

    First commit

---
+++ PerfectCountDown/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security;
+
+namespace PerfectCountDown
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Console.Write($"Start number: ");
+            int start = int.Parse(Console.ReadLine());
+            Console.Write($"Step number: ");
+            int step = int.Parse(Console.ReadLine());
+
+            switch(CheckIfLegal(start,step))
+            {
+                case 0:
+                
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+
+            }
+
+        }
+    }
+}
