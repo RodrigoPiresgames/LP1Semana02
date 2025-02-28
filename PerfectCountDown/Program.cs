898a755b20f14cdc515343910f3a3d279b678b1d
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-02-28 14:25:26 +0000

    Add verification process

--- PerfectCountDown/Program.cs
+++ PerfectCountDown/Program.cs
@@ -16,7 +16,7 @@ namespace PerfectCountDown
             switch(CheckIfLegal(start,step))
             {
                 case 0:
-                
+                    Console.WriteLine($"Step number: ");
                     break;
                 case 1:
 
@@ -30,6 +30,18 @@ namespace PerfectCountDown
 
             }
 
+        }
+
+        private static int CheckIfLegal(int x, int y)
+        {
+            if(x <= 1 || y <= 1 )
+                return 1;
+            else if(y >= x)
+                return 2;
+            else if(x % y != 0)
+                return 3;
+            else
+                return 0;
         }
     }
 }
