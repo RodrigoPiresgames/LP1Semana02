c9a4e1e63bca574031a3c12f953160133525069e
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-02-28 13:51:07 +0000

    Cilindro

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,15 @@ namespace Cilindro
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Gimme a");
+            string str = Console.ReadLine();
+            float a = float.Parse(str);
+            Console.WriteLine("Gimme r");
+            str = Console.ReadLine();
+            float r = float.Parse(str);
+
+            Console.WriteLine($"{ Math.PI * Math.Pow(r, 2) * a}");
+            Console.WriteLine($"{ (2 * Math.PI * r) * (r + a)}");
         }
     }
 }
