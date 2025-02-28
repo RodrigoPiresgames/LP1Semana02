940c3d50d20a96b555c8f205395ba775e469e940
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-02-28 13:55:38 +0000

    add 3 casas decimais

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -8,13 +8,13 @@ namespace Cilindro
         {
             Console.WriteLine("Gimme a");
             string str = Console.ReadLine();
-            float a = float.Parse(str);
+            double a = double.Parse(str);
             Console.WriteLine("Gimme r");
             str = Console.ReadLine();
-            float r = float.Parse(str);
+            double r = double.Parse(str);
 
-            Console.WriteLine($"{ Math.PI * Math.Pow(r, 2) * a}");
-            Console.WriteLine($"{ (2 * Math.PI * r) * (r + a)}");
+            Console.WriteLine($"{ Math.PI * Math.Pow(r, 2) * a:f3}");
+            Console.WriteLine($"{ (2 * Math.PI * r) * (r + a):f3}");
         }
     }
 }
