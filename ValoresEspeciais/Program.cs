d4961d8d3b42978efe2c3feae0dfdb72abfc24b2
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-02-28 13:58:24 +0000

    Add all known types

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,21 @@ namespace ValoresEspeciais
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine($"{sbyte.MaxValue} {sbyte.MinValue}");
+            Console.WriteLine($"{byte.MaxValue} {byte.MinValue}");
+            Console.WriteLine($"{short.MaxValue} {short.MinValue}");
+            Console.WriteLine($"{ushort.MaxValue} {ushort.MinValue}");
+            Console.WriteLine($"{int.MaxValue} {int.MinValue}");
+            Console.WriteLine($"{uint.MaxValue} {uint.MinValue}");
+            Console.WriteLine($"{long.MaxValue} {long.MinValue}");
+            Console.WriteLine($"{ulong.MaxValue} {ulong.MinValue}");
+            Console.WriteLine($"{float.MaxValue} {float.MinValue}");
+            Console.WriteLine($"{double.MaxValue} {double.MinValue}");
+            Console.WriteLine($"{decimal.MaxValue} {decimal.MinValue}");
+            Console.WriteLine($"{char.MaxValue} {char.MinValue}");
+
+            Console.WriteLine($"{double.PositiveInfinity} {double.NegativeInfinity}");
+            Console.WriteLine($"{float.NaN}");
         }
     }
 }
